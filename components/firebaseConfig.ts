cd48f6f3d1fac836fed57c5c7d1d4ad438c1ba41
Nikolas Negrão Pessoa <202024722@aluno.unb.br>
2026-04-28 12:27:08 -0300

    Divisao Rota/Design de fazer cadastro de usuarios

--- components/firebaseConfig.ts
+++ components/firebaseConfig.ts
@@ -1,17 +1,39 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+//import { initializeApp } from "firebase/app";
+//import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBBLgCUsNqbhufTirVUgYIg8ywyHpDakJs",
-  authDomain: "meau-adea2.firebaseapp.com",
-  projectId: "meau-adea2",
-  storageBucket: "meau-adea2.firebasestorage.app",
-  messagingSenderId: "896996881322",
-  appId: "1:896996881322:web:732ec24392a1bce84eb1d3",
-  measurementId: "G-G3QTNYJG68"
-};
+//const firebaseConfig = {
+//  apiKey: "AIzaSyBBLgCUsNqbhufTirVUgYIg8ywyHpDakJs",
+//  authDomain: "meau-adea2.firebaseapp.com",
+//  projectId: "meau-adea2",
+//  storageBucket: "meau-adea2.firebasestorage.app",
+//  messagingSenderId: "896996881322",
+//  appId: "1:896996881322:web:732ec24392a1bce84eb1d3",
+//  measurementId: "G-G3QTNYJG68"
+//};
 
-const app = initializeApp(firebaseConfig);
+//const app = initializeApp(firebaseConfig);
 
 // EXPORTAÇÃO DO AUTH
+//export const auth = getAuth(app);
+
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyChdJIP1-G-1S5YWmVlsSvCOZaWgwMw16c",
+  authDomain: "mecanismo-autenticacao.firebaseapp.com",
+  projectId: "mecanismo-autenticacao",
+  storageBucket: "mecanismo-autenticacao.firebasestorage.app",
+  messagingSenderId: "233087703566",
+  appId: "1:233087703566:web:1ed2429538fbc408a13a5f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
